fbdf01b166a643621f5bf98fa08a48c397b2c436
Arkadiy Rudin <arudin@ap.org>
2021-08-26 22:03:22 -0400

    test

--- src/com/arkrud/pokerconsole/TreeInterface/CustomTreeMouseListener.java
+++ src/com/arkrud/pokerconsole/TreeInterface/CustomTreeMouseListener.java
@@ -1,6 +1,6 @@
 package com.arkrud.pokerconsole.TreeInterface;
 
-import java.awt.Point;
+import java.awt.Point;  
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.beans.PropertyChangeEvent;
